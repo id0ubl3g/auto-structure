22b5253049215b02e29c6045c3a68cd57c2bb0d6
George Victor <id0ubl3g@gmail.com>
2024-10-23 05:56:43 -0300

    feat: create initial class structure for project setup

---
+++ src/modules/create_structure.py
@@ -0,0 +1,6 @@
+class CreateStructure:
+    def __init__(self):
+        self.root_directory: str = "projects"
+        self.subdirectories: dict = {}
+        self.directory_not_exists: bool = None
+        self.init_files: list = ['README.md', '.gitignore', '.env', '.env.example', 'LICENSE']
